73af29a97cfd7d29a88ff3e6f0f8767f84ebfbdb
spl0i7 <ketansingh.sh@gmail.com>
2018-10-25 17:29:57 +0530

    major changes to art/search page

--- src/views/Art.vue
+++ src/views/Art.vue
@@ -10,97 +10,155 @@
 
        <table class="table">
            <tbody>
-                <tr>
-                   <td><b>Name</b></td>
-                   <td>{{this.artJSON['name']}}</td>
-               </tr>
-               <tr>
+                <tr v-if="this.artJSON['curr_owner']">
                    <td><b>Owner</b></td>
                    <td>{{this.artJSON['curr_owner']}}</td>
                </tr>
-                <tr>
+                <tr v-if="this.artJSON['taxon_details_group']">
+                   <td><b>Classification</b></td>
+                   <td>{{this.artJSON['taxon_details_group'][0]['classification']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['accession_no']">
                    <td><b>Accession Number</b></td>
                    <td>{{this.artJSON['accession_no']}}</td>
                </tr>
-                <tr>
+                <tr v-if="this.artJSON['prod_pri_person_details_group']">
                    <td><b>Primary Maker</b></td>
                    <td>{{this.artJSON['prod_pri_person_details_group'][0]['prod_pri_person_name'] }}</td>
                </tr>
-                <tr>
-                   <td><b>Nationality</b></td>
-                   <td>{{this.artJSON['prod_pri_person_details_group'][0]['prod_pri_person_nationality']}}</td>
-               </tr>
-                <tr>
+                <tr v-if="this.artJSON['prod_pri_person_details_group'][0]['prod_pri_person_lifeyears']">
                    <td><b>Life Years</b></td>
                    <td>{{this.artJSON['prod_pri_person_details_group'][0]['prod_pri_person_lifeyears']}}</td>
                </tr>
-                <tr>
+                <tr v-if="this.artJSON['name']">
+                   <td><b>Name</b></td>
+                   <td>{{this.artJSON['name']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['prod_pri_date_details_group'][0]['prod_pri_date']">
                    <td><b>Primary Prod Date</b></td>
                    <td>{{this.artJSON['prod_pri_date_details_group'][0]['prod_pri_date']}}</td>
                </tr>
-                <tr>
-                   <td><b>Acquisition Source</b></td>
-                   <td>{{this.artJSON['acq_source_address_group'][0]['acq_source']}}</td>
+                <tr v-if="this.artJSON['prod_pri_place_group'][0]['prod_pri_place']">
+                   <td><b>Production Place</b></td>
+                   <td>{{this.artJSON['prod_pri_place_group'][0]['prod_pri_place']}}</td>
                </tr>
-                <tr>
-                   <td><b>Signature Date</b></td>
-                   <td>{{this.artJSON['signature_date']}}</td>
+                <tr v-if="this.artJSON['prod_pri_date_details_group'][0]['prod_pri_date']">
+                   <td><b>Production Date</b></td>
+                   <td>{{this.artJSON['prod_pri_date_details_group'][0]['prod_pri_date']}}</td>
+               </tr>                   
+                <tr v-if="this.artJSON['edition']">
+                   <td><b>Edition</b></td>
+                   <td>{{this.artJSON['edition']}}</td>
                </tr>
-                <tr>
+                <tr v-if="this.artJSON['material_desc']">
                    <td><b>Material</b></td>
                    <td>{{this.artJSON['material_desc']}}</td>
                </tr>
-                <tr>
+                <tr v-if="this.artJSON['measure_desc']">
                    <td><b>Measure</b></td>
                    <td>{{this.artJSON['measure_desc']}}</td>
                </tr>
-                <tr>
-                   <td><b>Acquisation Source</b></td>
-                   <td>{{this.artJSON['acq_source_address_group'][0]['acq_source']}}</td>
+                <tr v-if="this.artJSON['brief_desc']">
+                   <td><b>Brief Description</b></td>
+                   <td>{{this.artJSON['brief_desc']}}</td>
                </tr>
-                <tr>
-                   <td><b>Acquisition Date</b></td>
-                   <td>{{this.artJSON['acq_date']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Valuation</b></td>
-                   <td>{{this.artJSON['valuation_group'][0]['valuation']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Valuation Date</b></td>
-                   <td>{{this.artJSON['valuation_group'][0]['valuation_date']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Current Location</b></td>
-                   <td>{{this.artJSON['curr_loc_details_all_parts_group'][0]['curr_loc_all_parts']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Purchase Price</b></td>
-                   <td>{{this.artJSON['acq_price_original']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Insurance Value</b></td>
-                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_valuation']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Insurance Method</b></td>
-                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_method']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Insurance Date</b></td>
-                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_date']}}</td>
-               </tr>
-                <tr>
-                   <td><b>Insurance Valuer</b></td>
-                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_person']}}</td>
-               </tr>
-                <tr>
+               <tr v-if="this.artJSON['condition_desc']">
                    <td><b>Condition Description</b></td>
                    <td>
                        <p v-for="desc in getCondDesc()" :key="desc">{{ desc }}</p>
                     </td>
                </tr>
-               
+                <tr v-if="this.artJSON['curr_loc_details_all_parts_group'][0]['curr_loc_all_parts']">
+                   <td><b>Current Location</b></td>
+                   <td>{{this.artJSON['curr_loc_details_all_parts_group'][0]['curr_loc_all_parts']}}</td>
+               </tr>
+               <tr v-if="this.artJSON['prov_notes_group']">
+                   <td><b>Prov Notes Group</b></td>
+                   <td>
+                       <p v-for="note in artJSON['prov_notes_group']" :key="note['prov_date']"><span>{{note['prov_date']}}</span> - <span>{{note['prov_person']}}</span></p>
+                   </td>
+               </tr>
+                <tr v-if="this.artJSON['exhibition_notes']">
+                   <td><b>Exhibition Details</b></td>
+                   <td>{{this.artJSON['exhibition_notes']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['accession_date']">
+                   <td><b>Accession Date</b></td>
+                   <td>{{this.artJSON['accession_date']}}</td>
+               </tr>
+
+                <tr v-if="this.artJSON['acq_date']">
+                   <td><b>Acquisition Date</b></td>
+                   <td>{{this.artJSON['acq_date']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['acq_price_local']">
+                   <td><b>Acquisation Price</b></td>
+                   <td>{{this.artJSON['acq_price_local']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['acq_source_address_group'][0]['acq_source']">
+                   <td><b>Acquisation Source</b></td>
+                   <td>{{this.artJSON['acq_source_address_group'][0]['acq_source']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['acq_method']">
+                   <td><b>Acquisation Method</b></td>
+                   <td>{{this.artJSON['acq_method']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_date']">
+                   <td><b>Deaccession Date</b></td>
+                   <td>{{this.artJSON['deacc_date']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_notes']">
+                   <td><b>Deaccession Notes</b></td>
+                   <td>{{this.artJSON['deacc_notes']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_person']">
+                   <td><b>Transferred To</b></td>
+                   <td>{{this.artJSON['deacc_person']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_proceeds']">
+                   <td><b>Deaccession Proceeds</b></td>
+                   <td>{{this.artJSON['deacc_proceeds']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_reason']">
+                   <td><b>Deaccession Reason</b></td>
+                   <td>{{this.artJSON['deacc_reason']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_method']">
+                   <td><b>Deaccession Method</b></td>
+                   <td>{{this.artJSON['deacc_method']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['deacc_date_release']">
+                   <td><b>Deaccession Date of Release</b></td>
+                   <td>{{this.artJSON['deacc_date_release']}}</td>
+               </tr>        
+                <tr v-if="this.artJSON['prod_pri_date_details_group'][0]['prod_pri_date']">
+                   <td><b>Primary Prod Date</b></td>
+                   <td>{{this.artJSON['prod_pri_date_details_group'][0]['prod_pri_date']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['acq_source_address_group'][0]['acq_source']">
+                   <td><b>Acquisition Source</b></td>
+                   <td>{{this.artJSON['acq_source_address_group'][0]['acq_source']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['signature_date']">
+                   <td><b>Signature Date</b></td>
+                   <td>{{this.artJSON['signature_date']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['insurance_closing_amv_group']">
+                   <td><b>Insurance Method</b></td>
+                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_method']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['insurance_closing_amv_group']">
+                   <td><b>Insurance Date</b></td>
+                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_date']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['insurance_closing_amv_group']">
+                   <td><b>Insurance Value</b></td>
+                   <td>{{this.artJSON['insurance_closing_amv_group'][0]['ob_sym_closing_ins_valuation']}}</td>
+               </tr>
+                <tr v-if="this.artJSON['valuation_group']">
+                   <td><b>Market Value</b></td>
+                   <td>{{this.artJSON['valuation_group'][0]['valuation']}}</td>
+               </tr>
            </tbody>
        </table>
                   </div>
@@ -172,6 +230,9 @@ export default {
             }
             if(!data['prod_pri_date_details_group'][0]) {
                 data['prod_pri_date_details_group'] = [{}];
+            }
+            if(!data['prod_pri_place_group'][0]) {
+                data['prod_pri_place_group'] = [{}];
             }
             
 
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,67 +1,69 @@
 <template>
 
   <div>
-    <div class="row">
-      <h1>Search</h1>
-      <p>You can either do a quick search, search by accession number, search by name or search by artist</p>
-      <div class="col s12">
-        <div class="row center-align">
-          <div class="input-field col s4">
-            <i class="material-icons prefix">search</i>
-            <input id="icon_prefix" type="text" class="validate" placeholder="Search Text" v-model="searchQuery" v-on:keyup.enter="doSearch">
+    <loading-screen v-if="isLoading" loadingMessage="Loading all the collection"></loading-screen>
+    <div v-else>
+      <div class="row">
+        <div class="row">
+          <h3 class="col s6">{{$route.params.owner}}</h3>
+          <div class="input-field col s3">
+                <i class="material-icons prefix">search</i>
+                <input id="icon_prefix" type="text" class="validate" placeholder="Search Text" v-model="searchQuery" v-on:keyup.enter="doSearch">
           </div>
         </div>
-        <div class="row center-align">
-          <a class="waves-effect waves-light btn" @click="doSearch"><i class="material-icons left">search</i>Search</a>
+
+        <div class="col s12">
+          <div class="row center-align">
+            
+          </div>
         </div>
       </div>
+      <div class="center-align centeredContainer">
+        <table v-if="filteredCollections" class="striped table">
+          <!-- Accession number, Artist, Item, Medium, Location, Owner -->
+          <thead>
+            <td></td>
+            <td><b>Accession</b></td>
+            <td><b>Artist</b></td>
+            <td><b>Name</b></td>
+            <td><b>Location</b></td>
+            <td></td>
+          </thead>
+          <tbody>
+            <!-- /* eslint-disable */ -->
+            <tr v-for="rec in filteredCollections" :key="rec.access_no">
+              <td><img :src="rec.thumb"  class="artThumnail" ></td>
+              <td>{{rec.access_no}}</td>
+              <td>{{rec.artist}}</td>
+              <td>{{rec.name}}</td>
+              <td>{{rec.location}}</td>
+
+              <td> <router-link class="btn waves-effect waves-light" :to="{ name: 'art', params: { id: rec.id }}" >View</router-link></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    
-    <div class="center-align centeredContainer">
-      <table v-if="collections" class="striped table">
-        <!-- Accession number, Artist, Item, Medium, Location, Owner -->
-        <thead>
-          <td>Preview</td>
-          <td>Accession Number</td>
-          <td>Artist</td>
-          <td>Name</td>
-          <td>Location</td>
-
-          <td>View</td>
-        </thead>
-        <tbody>
-          <!-- /* eslint-disable */ -->
-          <tr v-for="rec in collections" :key="rec.access_no">
-            <td><img :src="rec.thumb"  class="artThumnail" ></td>
-            <td>{{rec.access_no}}</td>
-            <td>{{rec.artist}}</td>
-            <td>{{rec.name}}</td>
-            <td>{{rec.location}}</td>
-
-            <td> <router-link class="btn waves-effect waves-light" :to="{ name: 'art', params: { id: rec.id }}" >View</router-link></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-
   </div>
-
 </template>
 <script>
 
 import Search from "@/components/Search.vue";
+import LoadingScreen from "@/components/Loading.vue";
 
 
 export default {
   components: {
-    Search
+    Search,
+    LoadingScreen
   },
   data() {
     return {
       rawCollections : [],
       searchResults : null,
       searchQuery: null,
-      collections : []
+      collections : [],
+      isLoading: true,
     }
 
   },
@@ -69,6 +71,7 @@ export default {
     let ownerData = localStorage.getItem(this.$route.params.owner);
     if(ownerData) {
       this.collections = JSON.parse(ownerData);
+      this.isLoading = false;
     }
     else {
 
@@ -95,18 +98,36 @@ export default {
             }));
             
             localStorage.setItem(this.$route.params.owner, JSON.stringify(this.collections));
+            this.isLoading = false;
         });
       });
   
   
     }
   },
+  computed : {
+    filteredCollections : function() {
+      if(!this.searchQuery) return this.collections;
+      else {
+        let searchResults = [];
+        let reg = new RegExp(this.searchQuery, 'gi');
 
+        for(let i = 0 ; i < this.collections.length; i++) {
+          if( reg.test(this.collections[i]['location']) || reg.test(this.collections[i]['name']) || reg.test(this.collections[i]['artist']) || reg.test(this.collections[i]['access_no'])) {
+            searchResults.push(this.collections[i]);
+          }
+        
+        }
+
+        return searchResults;
+
+        
+      }
+    }
+  },
   methods: {
     doSearch : function() {
-
-      this.searchResults = null;
-      this.doAPISearch(this.searchQuery);
+      
     },
   }
 };
